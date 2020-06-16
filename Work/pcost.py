8ff64c69c75af320a29ba1149502ac5499f5ea97
rapm665 <jacob.lew2020@gmail.com>
2020-06-16 08:11:35 -0700

    added csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,23 @@
 # pcost.py
 #
 # Exercise 1.27
-
+import csv
 def portfolio_cost(filename): 
-    shares = 0
-    price = 0
-    total = 0
-    f = open(filename)
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        shares = row[1]
-        price = row[2]
-        total += float(price) * int(shares)
-    f.close()
-    return total
+    try:
+        shares = 0
+        price = 0
+        total = 0
+        f = open(filename)
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            #i = row.split(',')
+            shares = row[1]
+            price = row[2]
+            total += float(price) * int(shares)
+        f.close()
+        return total
+    except ValueError:
+        print("Doesn't look good, dawg", line)
 cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost: ', cost)
