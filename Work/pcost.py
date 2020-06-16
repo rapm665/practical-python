eb218fa4f8ea5ba7b460b7bea76d58950fadfa3b
rapm665 <jacob.lew2020@gmail.com>
2020-06-15 20:32:03 -0700

    Modified program to a function call as per Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,18 @@
 #
 # Exercise 1.27
 
-shares = 0
-price = 0
-total = 0
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-for line in f:
-    row = line.split(',')
-    shares = row[1]
-    price = row[2]
-    total += float(price) * int(shares)
-print('Total cost ', total)
-f.close()
+def portfolio_cost(filename): 
+    shares = 0
+    price = 0
+    total = 0
+    f = open(filename)
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        shares = row[1]
+        price = row[2]
+        total += float(price) * int(shares)
+    f.close()
+    return total
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost: ', cost)
